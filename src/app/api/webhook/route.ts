a447a625911efefc9bdae6521ec686f673463926
Marwan Hisham <marwanhiisham@outlook.com>
2024-06-29 05:16:17 +0300

    feat((webhook)/route.ts): webhook done

---
+++ src/app/api/webhook/route.ts
@@ -0,0 +1,41 @@
+import { stripe } from "@/lib/stripe/stripe"
+import Stripe from "stripe"
+import { NextResponse, type NextRequest } from "next/server"
+import { createClient } from "@/lib/supabase/server"
+import { randomUUID } from "crypto"
+
+export async function POST(req: NextRequest) {
+  const signature = req.headers.get("stripe-signature") ?? ""
+  const supabase = createClient()
+  try {
+    let event: Stripe.Event
+    event = stripe.webhooks.constructEvent(
+      await req.text(),
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET!
+    )
+    const session = event.data.object as Stripe.Checkout.Session
+
+    if (event.type === "payment_intent.amount_capturable_updated") {
+      // Create a new booking in Supabase with status 'pending'
+      const { error } = await supabase
+        .from("bookings")
+        .insert([
+          {
+            tour_id: randomUUID(),
+            user_email: "abbbas@gmail.com",
+            status: "pending",
+            session_id: session.id,
+          },
+        ])
+        .select()
+      if (error) {
+        return { error: error.message }
+      }
+    }
+    return NextResponse.json({ status: 200, event: event })
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json({ status: "Failed" })
+  }
+}
--- src/server/routers/booking.ts
+++ src/server/routers/booking.ts
@@ -1,14 +1,12 @@
 import { createTRPCRouter, publicProcedure } from ".."
 import { z } from "zod"
 import { createClient } from "@/lib/supabase/server"
-import { randomUUID } from "crypto"
 import { stripe } from "@/lib/stripe/stripe"
 export const bookingRouter = createTRPCRouter({
   bookTrip: publicProcedure
     .input(z.number())
     .mutation(async ({ input, ctx }) => {
       // Create Supabase Server Client
-      const supabase = createClient()
 
       const session = await stripe.checkout.sessions.create({
         payment_method_types: ["card"],
@@ -29,26 +27,10 @@ export const bookingRouter = createTRPCRouter({
           capture_method: "manual",
         },
 
-        success_url: `${ctx.headers.get("origin")}/success?session_id={CHECKOUT_SESSION_ID}`,
+        success_url: `${ctx.headers.get("origin")}/success`,
         cancel_url: `${ctx.headers.get("origin")}`,
       })
-
-      // Create a new booking in Supabase with status 'pending'
-      const { error } = await supabase
-        .from("bookings")
-        .insert([
-          {
-            tour_id: randomUUID(),
-            user_email: "abbbas@gmail.com",
-            status: "pending",
-            session_id: session.id,
-          },
-        ])
-        .select()
-
-      if (error) {
-        return { error: error.message }
-      }
+      console.log(session)
 
       return { sessionId: session.id }
     }),
@@ -58,19 +40,17 @@ export const bookingRouter = createTRPCRouter({
     const { data: booking, error } = await supabase
       .from("bookings")
       .update({ status: "approved" })
-      .eq("id", 37)
+      .eq("id", 45)
       .select()
 
     if (error) {
       console.log("Server", error)
       return error
     }
-    const session = await stripe.checkout.sessions.retrieve(
+
+    const paymentIntent = await stripe.paymentIntents.capture(
       booking[0].session_id
     )
-    const paymentIntentId = session.payment_intent as string
-
-    const paymentIntent = await stripe.paymentIntents.capture(paymentIntentId)
     return { booking, paymentIntent }
   }),
 })
